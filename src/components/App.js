3b27d8d8c034ae0c29a7f0c738bfb5cdb0a6aac6
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-06-21 15:31:40 -0500

    app is up and running

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <p>im working for {this.props.name} and making a ToDo app with redux</p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import App from './components/App';
+const store = createStore(rootReducer);
 
-
-ReactDOM.render(<App name={'Jason Duncan'} />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
