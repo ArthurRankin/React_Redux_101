1511479072b51b741a57232468ea3c55bf75f139
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-06-21 14:45:29 -0500

    file and folder setup complete

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>im working for {this.props.name} and making a ToDo app with redux</p>
+      </div>
+    );
+  }
+}
+
+export default App;
